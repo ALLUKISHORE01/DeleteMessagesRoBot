6ad7fc8d4450778337bde444aa1a4cf2cf0c1ed0
ALLUKISHORE01 <106458056+ALLUKISHORE01@users.noreply.github.com>
2022-05-29 18:58:54 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -79,8 +79,8 @@ def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
 
 
-REQD_PERMISSIONS = "https://t.me/SpEcHlDe/857"
-GIT_REPO_LINK = "https://github.com/SpEcHiDe/DeleteMessagesRoBot"
+REQD_PERMISSIONS = "https://t.me/Virab_Bot_Updates/34"
+GIT_REPO_LINK = "@AlluKishore_Official"
 """ strings to be used in the bot """
 START_MESSAGE = get_config("START_MESSAGE", (
     "I'm a bot that can delete <s>all</s> "
